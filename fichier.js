1c42bb0a55a02acfad98122da244220d63740fae
Ameyibo_gator <adjoffrey@gmail.com>
2018-01-17 15:47:16 +0000

    Commentaire sur les 3 derniers alerts et decommentaire de l instruction for

--- fichier.js
+++ fichier.js
@@ -9,12 +9,12 @@ function tst(){
 
 
 	// maintenant on veut un objet à partir d'une chaine JSON
-	/*var objet2 = JSON.parse(chaine); // renvoie un objet ayant la même structure que la variable objet ci-dessus
+	var objet2 = JSON.parse(chaine); // renvoie un objet ayant la même structure que la variable objet ci-dessus
 	for (i in objet2) {
 		alert(objet2.i);
-	};*/
-	alert(objet2);
-	alert(objet2.variable1); // affiche : "une chaine"
-	alert(objet2.variable2); // affiche : 5
+	};
+	//alert(objet2);
+	//alert(objet2.variable1); // affiche : "une chaine"
+	//alert(objet2.variable2); // affiche : 5
 }
 	
